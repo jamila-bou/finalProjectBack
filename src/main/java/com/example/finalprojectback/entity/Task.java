ac8b3309a0c001695a46243032a63c8b7ce0bc7c
jamila-bou <boularesjamila10@gmail.com>
2025-07-18 12:55:34 +0100

    first commit

--- src/main/java/com/example/finalprojectback/entity/Task.java
+++ src/main/java/com/example/finalprojectback/entity/Task.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -21,9 +23,9 @@ public class Task {
     private String title;
 
     private String content;
-
+@CreationTimestamp
     private LocalDateTime createdAt;
-
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     public Long getId() {
